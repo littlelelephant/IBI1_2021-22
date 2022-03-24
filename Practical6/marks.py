69f5cc94805748d2b7e571fb4794ca8d9332f0da
littlelelephant <d1981329282@126.com>
2022-03-24 18:59:53 +0800

    edit marks.py

--- Practical6/marks.py
+++ Practical6/marks.py
@@ -9,3 +9,14 @@ df = pd.DataFrame(marks) #use the data in the list
 df.plot.box(title="Distribution of Marks") #give the boxplot a title
 plt.grid(linestyle="--", alpha=0.3) #write some parameters about the boxplot
 plt.show() #show the boxplot
+
+m=len(marks) #let m equals to the elements number in the list
+i=0
+j=0
+while i<m:
+    j+=marks[i] #get the sum of all marks
+    i+=1 #to calculate the next element in the list
+print('the average mark is',j/m) #show the average mark.Obviously is smaller than 60%
+
+
+

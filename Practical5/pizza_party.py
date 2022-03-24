fe5e0fafcdd913e6f93c7eae5d395bacd90a4f42
littlelelephant <d1981329282@126.com>
2022-03-24 14:03:18 +0800

    edit pizza_party.py

---
+++ Practical5/pizza_party.py
@@ -0,0 +1,5 @@
+p=0 #give p a definition
+for n in range (1,13): #I use n=12 as a example, we can calculate the total number of pizza slices by changing the number in brackets 
+    p=(n**2+n+2)/2 #use the equation which given us
+    print('the total number of pizza slices at',n,'cuts is',p) #print the results
+#From the results we can find that 11cuts is required to make a slice for each member of the IBI1 class
